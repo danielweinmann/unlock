730b9b44bf613fe2e7cdf200e0ae74c64960aaeb
Daniel Weinmann <danielweinmann@gmail.com>
2014-09-24 17:54:52 -0300

    New round of layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( icomoon-free/icomoon.eot )
+Rails.application.config.assets.precompile += %w( icomoon-free/icomoon.woff )
+Rails.application.config.assets.precompile += %w( icomoon-free/icomoon.ttf )
