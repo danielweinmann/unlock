5a3308351e21bfc9d634e0bbe489dcfd922b83f2
Daniel Weinmann <danielweinmann@gmail.com>
2014-09-29 01:56:07 -0300

    Configuring Google Analytics

--- app/decorators/contribution_decorator.rb
+++ app/decorators/contribution_decorator.rb
@@ -6,11 +6,4 @@ module ContributionDecorator
     self.value.to_i
   end
   
-  def margin
-    return "0" unless self.initiative.image.width && self.initiative.image.height
-    ratio = self.initiative.image.height.to_f / self.initiative.image.width
-    return "0" if ratio <= 0.191
-    "-#{(ratio / 2.0 * 100) - 19.1}%"
-  end
-
 end
