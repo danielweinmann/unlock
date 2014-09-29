bdb1751c60bd361115132b1ca7c673ab47a8b87e
Daniel Weinmann <danielweinmann@gmail.com>
2014-09-29 17:34:17 -0300

    Not showing initiatives on sandbox on homepage

--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -20,6 +20,10 @@ class Initiative < ActiveRecord::Base
     where("moip_token IS NOT NULL AND moip_token <> '' AND moip_key IS NOT NULL AND moip_key <> '' AND permalink IS NOT NULL AND permalink <> ''").order("updated_at DESC")
   end
   
+  def self.not_sandbox
+    where('NOT sandbox')
+  end
+  
   require 'redcloth'
 
   AutoHtml.add_filter(:redcloth).with({}) do |text, options|
