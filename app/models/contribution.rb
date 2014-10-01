f7259196bf8b4003f33c9f2500f1470a7d2a75e4
Daniel Weinmann <danielweinmann@gmail.com>
2014-09-30 22:59:46 -0300

    Not requiring address_complement

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -63,7 +63,7 @@ class Contribution < ActiveRecord::Base
   
   def presence_of_user_attributes
     return unless self.user.present? && self.initiative.present?
-    %w(full_name document phone_area_code phone_number birthdate address_street address_number address_complement address_district address_city address_state address_zipcode).each do |attribute|
+    %w(full_name document phone_area_code phone_number birthdate address_street address_number address_district address_city address_state address_zipcode).each do |attribute|
       unless self.user.attributes[attribute].present?
         self.errors.add("user.#{attribute}", "não pode ficar em branco")
       end
