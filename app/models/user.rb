85dd85d848cc96e8463d789c054945fb9db99d9a
Daniel Weinmann <danielweinmann@gmail.com>
2014-10-26 18:21:17 -0200

    Cleaning up User columns before validation instead of before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   validates :document, cpf: true, allow_nil: true, allow_blank: true
 
-  before_save do
+  before_validation do
     self.name = self.email.match(/(.+)@/)[1] unless self.name.present?
     self.document = self.document.scan(/\d/).join('') if self.document
     self.phone_area_code = self.phone_area_code.scan(/\d/).join('') if self.phone_area_code
