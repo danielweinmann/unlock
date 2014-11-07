a8d9955ec881e27144b5dd94b32feb99b1bf9139
Diogo Biazus <diogob@gmail.com>
2014-11-07 18:28:29 -0500

    Adds basic initiative spec with associations and validations assertions

---
+++ spec/models/initiative_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Initiative, type: :model do
+  describe "asociations" do
+    it{ is_expected.to belong_to :user }
+    it{ is_expected.to have_many :contributions }
+    it{ is_expected.to have_many :gateways }
+  end
+
+  describe "validations" do
+
+    it{ is_expected.to validate_presence_of :user }
+    it{ is_expected.to validate_uniqueness_of :permalink }
+    it{ is_expected.to allow_value('foo', nil).for(:permalink) }
+    it{ is_expected.to_not allow_value('9').for(:permalink) }
+  end
+end
