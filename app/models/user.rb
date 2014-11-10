90bc602428feb067cb6e3f7def11d24a369f5b79
Diogo Biazus <diogob@gmail.com>
2014-11-10 14:28:00 -0500

    Refactored before_validation hook into smaller methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  ONLY_DIGITS_ATTRIBUTES = %w(document phone_area_code phone_number address_zipcode)
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   has_many :initiatives
@@ -7,11 +8,19 @@ class User < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
-  before_validation do
+  before_validation :extract_name_from_email
+  before_validation :extract_digits_from_attributes
+
+  private
+  def extract_name_from_email
     self.name = self.email.match(/(.+)@/)[1] unless self.name.present?
-    %w(document phone_area_code phone_number address_zipcode).each do |attribute|
-      self.send("#{attribute}=", self.send(attribute).scan(/\d/).join('')) if self.send(attribute)
-    end
   end
 
+  def extract_digits_from_attributes
+    ONLY_DIGITS_ATTRIBUTES.each{|attribute| extract_digits_from attribute }
+  end 
+
+  def extract_digits_from attribute
+    self.attributes[attribute] && self.attributes[attribute].gsub!(/[^\d]/, '')
+  end
 end
