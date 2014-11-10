f30b7c30fe6f00b44e5a0ec0a6e0b846aa6a394b
Daniel Weinmann <danielweinmann@gmail.com>
2014-11-10 15:09:46 -0200

    Refactoring User validations to avoid duplication of code

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,9 @@ class User < ActiveRecord::Base
 
   before_validation do
     self.name = self.email.match(/(.+)@/)[1] unless self.name.present?
-    self.document = self.document.scan(/\d/).join('') if self.document
-    self.phone_area_code = self.phone_area_code.scan(/\d/).join('') if self.phone_area_code
-    self.phone_number = self.phone_number.scan(/\d/).join('') if self.phone_number
-    self.address_zipcode = self.address_zipcode.scan(/\d/).join('') if self.address_zipcode
+    %w(document phone_area_code phone_number address_zipcode).each do |attribute|
+      self.send("#{attribute}=", self.send(attribute).scan(/\d/).join('')) if self.send(attribute)
+    end
   end
 
 end
