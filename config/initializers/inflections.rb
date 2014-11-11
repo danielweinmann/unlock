2bf74627e013516768dc1dc18df57e5f24d2214d
Daniel Weinmann <danielweinmann@gmail.com>
2014-11-11 05:32:24 -0200

    All views I18n'd

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -5,6 +5,7 @@
 # locales as you wish. All of these examples are active by default:
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular 'apoia', 'apoiam'
+  inflect.irregular 'meio de pagamento', 'meios de pagamento'
 end
 
 # These inflection rules are supported but not enabled by default:
