771657b62c79fd312c0845e28184adb36232c884
Diogo Biazus <diogob@gmail.com>
2014-11-17 22:22:50 -0500

    Adds specs for some contribution state machine methods. Fixed data truncation after spec run so we don't need those find_or_create calls

--- spec/support/seed_database.rb
+++ spec/support/seed_database.rb
@@ -1,14 +1,36 @@
 RSpec.configure do |config|
+  module TestGateway
+    module Models
+      module Gateway
+      end
+
+      module Contribution
+      end
+    end
+  end
+
   def get_user(name)
     @users ||= {}
-    @users[name] ||= User.create_with(email: "#{name}@bar.com", password: 'test123', password_confirmation: 'test123', locale: 'en').find_or_create_by!(name: name.humanize)
+    @users[name] ||= User.create!(email: "#{name}@bar.com", password: 'test123', password_confirmation: 'test123', locale: 'en', name: name.humanize)
     @users[name].reload
   end
 
   def get_initiative(permalink, user)
     @initiatives ||= {}
-    @initiatives[permalink] ||= Initiative.create_with(name: permalink.humanize, user: user).find_or_create_by!(permalink: permalink)
+    @initiatives[permalink] ||= Initiative.create!(name: permalink.humanize, user: user, permalink: permalink)
     @initiatives[permalink].reload
+  end
+
+  def get_contribution(state)
+    @contributions ||= {}
+    @contributions[state] ||= Contribution.create!(user: visitor_user, initiative: unlock_initiative, value: 10, state: state, gateway: production_gateway)
+    @contributions[state].reload
+  end
+
+  def get_gateway(state)
+    @gateways ||= {}
+    @gateways[state] ||= Gateway.create!(initiative: unlock_initiative, module_name: 'TestGateway', state: state)
+    @gateways[state].reload
   end
 
   def the_creator_user
@@ -23,6 +45,18 @@ RSpec.configure do |config|
     get_initiative('unlock', the_creator_user)
   end
 
+  %w(pending active suspended).each do |state|
+    define_method("#{state}_contribution") do
+      get_contribution(state)
+    end
+  end
+
+  %w(draft sandbox production).each do |state|
+    define_method("#{state}_gateway") do
+      get_gateway(state)
+    end
+  end
+
   config.before(:all) do
     # Insert all data needed for tests
     the_creator_user
@@ -31,6 +65,8 @@ RSpec.configure do |config|
   end
 
   config.after(:all) do
-    ActiveRecord::Base.connection.execute "TRUNCATE users CASCADE"
+    %w(users contributions gateways initiatives).each do |table|
+      ActiveRecord::Base.connection.execute "TRUNCATE #{table} CASCADE"
+    end
   end
 end
