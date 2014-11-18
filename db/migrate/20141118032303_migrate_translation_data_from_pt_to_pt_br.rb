c012db61a9486f23d45d0ac59c96faffa15bb015
Daniel Weinmann <danielweinmann@gmail.com>
2014-11-18 01:39:05 -0200

    Updates migration to convert to ::text

--- db/migrate/20141118032303_migrate_translation_data_from_pt_to_pt_br.rb
+++ db/migrate/20141118032303_migrate_translation_data_from_pt_to_pt_br.rb
@@ -1,10 +1,10 @@
 class MigrateTranslationDataFromPtToPtBr < ActiveRecord::Migration
   def up
-    execute "UPDATE gateways SET title_translations = title_translations || ('pt-BR' => (title_translations -> 'pt')::text)"
-    execute "UPDATE gateways SET ordering_translations = ordering_translations || ('pt-BR' => (ordering_translations -> 'pt')::text)"
-    execute "UPDATE initiatives SET name_translations = name_translations || ('pt-BR' => (name_translations -> 'pt')::text)"
-    execute "UPDATE initiatives SET first_text_translations = first_text_translations || ('pt-BR' => (first_text_translations -> 'pt')::text)"
-    execute "UPDATE initiatives SET second_text_translations = second_text_translations || ('pt-BR' => (second_text_translations -> 'pt')::text)"
+    execute "UPDATE gateways SET title_translations = title_translations || ('pt-BR'::text => (title_translations -> 'pt')::text)"
+    execute "UPDATE gateways SET ordering_translations = ordering_translations || ('pt-BR'::text => (ordering_translations -> 'pt')::text)"
+    execute "UPDATE initiatives SET name_translations = name_translations || ('pt-BR'::text => (name_translations -> 'pt')::text)"
+    execute "UPDATE initiatives SET first_text_translations = first_text_translations || ('pt-BR'::text => (first_text_translations -> 'pt')::text)"
+    execute "UPDATE initiatives SET second_text_translations = second_text_translations || ('pt-BR'::text => (second_text_translations -> 'pt')::text)"
   end
   def down
     execute "UPDATE gateways SET title_translations = delete(title_translations, 'pt-BR')"
