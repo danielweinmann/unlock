69ef73659e15389df4c6ba9f75999e841401e062
Daniel Weinmann <danielweinmann@gmail.com>
2014-11-18 01:44:57 -0200

    Changes => operator to hstore function in migration

--- db/migrate/20141118032303_migrate_translation_data_from_pt_to_pt_br.rb
+++ db/migrate/20141118032303_migrate_translation_data_from_pt_to_pt_br.rb
@@ -1,10 +1,10 @@
 class MigrateTranslationDataFromPtToPtBr < ActiveRecord::Migration
   def up
-    execute "UPDATE gateways SET title_translations = title_translations || ('pt-BR'::text => (title_translations -> 'pt')::text)"
-    execute "UPDATE gateways SET ordering_translations = ordering_translations || ('pt-BR'::text => (ordering_translations -> 'pt')::text)"
-    execute "UPDATE initiatives SET name_translations = name_translations || ('pt-BR'::text => (name_translations -> 'pt')::text)"
-    execute "UPDATE initiatives SET first_text_translations = first_text_translations || ('pt-BR'::text => (first_text_translations -> 'pt')::text)"
-    execute "UPDATE initiatives SET second_text_translations = second_text_translations || ('pt-BR'::text => (second_text_translations -> 'pt')::text)"
+    execute "UPDATE gateways SET title_translations = title_translations || hstore('pt-BR', (title_translations -> 'pt'))"
+    execute "UPDATE gateways SET ordering_translations = ordering_translations || hstore('pt-BR', (ordering_translations -> 'pt'))"
+    execute "UPDATE initiatives SET name_translations = name_translations || hstore('pt-BR', (name_translations -> 'pt'))"
+    execute "UPDATE initiatives SET first_text_translations = first_text_translations || hstore('pt-BR', (first_text_translations -> 'pt'))"
+    execute "UPDATE initiatives SET second_text_translations = second_text_translations || hstore('pt-BR', (second_text_translations -> 'pt'))"
   end
   def down
     execute "UPDATE gateways SET title_translations = delete(title_translations, 'pt-BR')"
