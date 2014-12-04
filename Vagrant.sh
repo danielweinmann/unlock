3f6fe49685a3b3010944f597a8ab25d52ff605d7
Carlos Villela <cvillela@thoughtworks.com>
2014-12-04 00:04:00 -0200

    Clean up Vagrant provisioning config.
    Made a few adjustments so less network is used (and more is logged
    while the script is at it, for those often on crappy airport wifi
    networks like me :)
    
    Additionally, the script is mostly idempotent now, so that if you
    run `vagrant provision` again, it shouldn't blow up or do anything
    unwanted.

--- Vagrant.sh
+++ Vagrant.sh
@@ -1,35 +1,31 @@
-echo updating package information
-apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
-apt-get -y update >/dev/null 2>&1
-apt-get -y upgrade >/dev/null 2>&1
+#!/usr/bin/env bash
+set -o pipefail
+set -e
+set -x
 
-echo installing development tools
-apt-get -y install build-essential >/dev/null 2>&1
+if [ ! -f '/etc/apt/sources.list.d/brightbox-ruby-ng-trusty.list' ]; then
+  apt-add-repository -y ppa:brightbox/ruby-ng
+fi
 
-echo installing Git
-apt-get -y install git >/dev/null 2>&1
+apt-get -y update
+apt-get -y upgrade
 
-echo installing Ruby
-apt-get -y install ruby2.0 ruby2.0-dev >/dev/null 2>&1
+update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
+
+apt-get -y install build-essential git ruby2.0 ruby2.0-dev postgresql postgresql-contrib libpq-dev nodejs
+
 rm /usr/bin/ruby /usr/bin/gem /usr/bin/irb /usr/bin/rdoc /usr/bin/erb
 ln -s /usr/bin/ruby2.0 /usr/bin/ruby
 ln -s /usr/bin/gem2.0 /usr/bin/gem
 ln -s /usr/bin/irb2.0 /usr/bin/irb
 ln -s /usr/bin/rdoc2.0 /usr/bin/rdoc
 ln -s /usr/bin/erb2.0 /usr/bin/erb
-update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
 
-echo installing Bundler
-gem install bundler -N >/dev/null 2>&1
-# apt-get -y install bundler >/dev/null 2>&1
+gem list bundler | grep -q bundler
+if [ $? -ne 0 ]; then
+  gem install bundler -N
+fi
 
-echo installing PostGreSQL
-apt-get -y install postgresql postgresql-contrib libpq-dev >/dev/null 2>&1
-sudo -u postgres createuser --superuser vagrant
+sudo -u postgres createuser --superuser vagrant || true
 
-echo installing ExecJs runtime
-apt-get -y install nodejs >/dev/null 2>&1
-
-echo installing project Gems
-cd /vagrant
-bundle >/dev/null 2>&1
+cd /vagrant && bundle install
